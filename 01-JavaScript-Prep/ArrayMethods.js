d3bfd790dcffad4ac36838ad147badbeb9450dad
Prathap <130729696+Prathap-Thunga@users.noreply.github.com>
2026-01-27 18:29:52 +0000

    Array methods and output questions

--- 01-JavaScript-Prep/ArrayMethods.js
+++ 01-JavaScript-Prep/ArrayMethods.js
@@ -1,5 +1,15 @@
 // What is Map();
 // The map() method creates a new array populated with the results of calling a provided function on every element in the calling array.
+// pollyfill for map method
+//map.((currenItem, i, array)={})
+
+Array.prototype.myMap = function (Callback) {
+  const result = [];
+  for (let i = 0; i < this.length; i++) {
+    result.push(Callback(this[i], i, this));
+  }
+  return result;
+};
 
 const nums = [1, 2, 3, 4, 5];
 const multiply = nums.map((num, index, arry) => {
@@ -16,6 +26,18 @@ console.log(multiply);
 // What is Filter();
 // The filter() method creates a new array with all elements that pass the test implemented by the provided function.
 
+// pollyfill for filter method
+
+Array.prototype.myFilter = function (callback) {
+  const result = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) {
+      result.push(this[i]);
+    }
+  }
+  return result;
+};
+
 const ages = [32, 15, 19, 12, 25, 16, 8];
 const agesCanVote = ages.filter((ages) => {
   return ages >= 18;
@@ -26,8 +48,18 @@ console.log(agesCanVote);
 // What is Reduce();
 // The reduce() method executes a reducer function(that you provide) on each element of the array, resulting in a single output value.
 
+Array.prototype.myReduce = function (callback, initialValue) {
+  let accumulator = initialValue;
+  for (let i = 0; i < this.length; i++) {
+    accumulator = accumulator
+      ? callback(accumulator, this[i], i, this)
+      : this[i];
+  }
+  return accumulator;
+};
+
 const numbers = [1, 2, 3, 4, 5];
-const sum = numbers.reduce((accumulator, currentValue, i, arr) => {
+const sum = numbers.myReduce((accumulator, currentValue, i, arr) => {
   return accumulator + currentValue;
 }, 0);
 // 0 + 1 = 1
@@ -37,3 +69,24 @@ const sum = numbers.reduce((accumulator, currentValue, i, arr) => {
 // 10 + 5 = 15
 
 console.log(sum);
+
+let students = [
+  { name: "Alice", score: 85 },
+  { name: "Bob", score: 92 },
+  { name: "Charlie", score: 78 },
+  { name: "David", score: 90 },
+];
+
+//
+
+let stdNames = [];
+for (let i = 0; i < students.length; i++) {
+  stdNames.push(students[i].name.toUpperCase());
+}
+console.log(stdNames);
+
+const mapStuds = students.map((curr, i, arry) => {
+  return curr.name.toUpperCase();
+});
+
+console.log(mapStuds);
