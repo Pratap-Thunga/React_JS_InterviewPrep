680dc22a93bf3eb8f46528ffd67c23c022b48f3f
Prathap <130729696+Prathap-Thunga@users.noreply.github.com>
2026-01-27 21:39:49 +0000

    Function

---
+++ 01-JavaScript-Prep/Functions.js
@@ -0,0 +1,60 @@
+console.log("Functions is loaded");
+
+// What is function declaration -- Declaring a function. This can also be called defining a function(Function Definition)
+
+function greet() {
+  console.log("Hello World");
+}
+
+// Invoking or calling a function
+greet();
+greet();
+greet(); // calling multiple times
+
+//Q2 - What uis function expression -- Storing a function inside a variable or assigning a function to a variable
+
+const sayHi = function () {
+  // Function without a name is called anonymous function
+  console.log("Hi There");
+};
+sayHi();
+
+// Q3 - First Class Functions -- In JavaScript, functions are treated as first-class citizens. This means that functions can be assigned to variables, passed as arguments to other functions, and returned from other functions.
+function add(num) {
+  return num + num;
+}
+
+function getNum(func) {
+  return console.log("Sum is " + func(5));
+}
+
+getNum(add);
+
+// Q4 - What is IIFE (Immediateli Invoked Function Expression) -- A function runs as soons as it is defined.
+(function () {
+  console.log("IIFE Ran..");
+})();
+
+// Q5- IIEFE Output based questions
+
+function outer() {
+  let count = 0;
+  return function inner() {
+    count++;
+    console.log("Count is " + count);
+  };
+}
+
+const counter = outer();
+counter(); // Count is 1
+counter(); // Count is 2
+counter(); // Count is 3
+
+(function outFunc(x) {
+  return (
+    function inFunc(y) {
+      console.log(x);
+    },
+    20
+  );
+})(10); // Output is 10
